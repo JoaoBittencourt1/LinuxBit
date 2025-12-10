bf5960a6ed2d33c6037206895f7537b021fadb3c
joao <jvabgo@gmail.com>
2025-12-10 18:24:45 -0300

    feat: adicionando a janela de cada distro, da pra ver a logo, descrição e um link de download, fiz uma classe generica pra ser mais facil em vez de ficar criando varios arquivos xml, mas ainda nao fiz todas as distros

--- LinuxHub/MainWindow.xaml.cs
+++ LinuxHub/MainWindow.xaml.cs
@@ -11,14 +11,24 @@ using System.Windows.Shapes;
 
 namespace LinuxHub
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+    
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+
+            UbuntuPanel.MouseLeftButtonUp += (s, e) =>
+            {
+                string name = "Ubuntu";
+                string description = "O Ubuntu é uma distribuição Linux baseada no Debian, conhecida por sua estabilidade e facilidade de uso.";
+                string imagePath = "pack://application:,,,/Assets/Images/ubuntu.png";
+                string downloadLink = "https://ubuntu.com/download/desktop"; // pq karalhos minha net tinha que ficar ruim justo quando testo a porra de um site
+
+                DistroWindow window = new DistroWindow(name, description, imagePath, downloadLink);
+                window.Show();
+            };
+
         }
     }
 }
